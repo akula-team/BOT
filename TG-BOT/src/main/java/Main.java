8891a214e507c7fd94253e642530ad1e2205188b
akula-team <148052294+akula-team@users.noreply.github.com>
2023-10-16 23:45:56 +0500

    Add files via upload

---
+++ TG-BOT/src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+        String botName = "*";
+        String botToken = "***";
+        TelegramBotsApi telegramBotsApi = null;
+        try {
+            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new MyBot(botName, botToken));
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
