0599ca7be1d98511ee1dc274a3ac26eb757eaa47
akula-team <148052294+akula-team@users.noreply.github.com>
2023-10-17 12:02:30 +0500

    Update Main.java

--- TG-BOT/src/main/java/Main.java
+++ TG-BOT/src/main/java/Main.java
@@ -6,7 +6,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class Main {
     public static void main(String[] args) {
         String botName = "Echo bot";
-        String botToken = "6348425588:AAEsYA7zZYekv4QNyYbuRUNkpVEH6OjSyic";
+        String botToken = "";
         TelegramBotsApi telegramBotsApi = null;
         try {
             telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
